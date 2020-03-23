80cb205eb8a00e5e050ab835b43ce7d0670d1c43
Daniel Oseguera <codigito.c@gmail.com>
2020-03-22 19:13:58 -0600

    .

--- src/infrastructure/middlewares/PermissionMiddleware.ts
+++ src/infrastructure/middlewares/PermissionMiddleware.ts
@@ -3,8 +3,8 @@ import TYPES from "../../types";
 import { SignInUseCase } from "../../core/application/usecases/auth/SignInUseCase";
 import { FindByIdUserUseCase } from "../../core/application/usecases/users/FindByIdUserUseCase";
 
-export default function permit(permission): Express.Response {
-    return async (request, response, next): Promise<Express.Response> => {
+export default function permit(permission) {
+    return async (request, response, next) => {
         const authUseCase = container.get<SignInUseCase>(TYPES.SignInUseCase);
         const findUserByIdUseCase = container.get<FindByIdUserUseCase>(TYPES.FindByIdUserUseCase);
 
@@ -17,7 +17,7 @@ export default function permit(permission): Express.Response {
             const permissions = await findUserByIdUseCase.execute(1).then( userDTO => userDTO.role.permissions);
             for (const key of permissions) {
                 if (permission === key.name) {
-                    return next();
+                    next();
                 }
             }
             return response.status(401).json({message: "User unauthorized"});
